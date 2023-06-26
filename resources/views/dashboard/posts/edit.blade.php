ea9ed6d3162d2cd821d1e9be9b36d25e66d20948
Nando118 <fernandoverdysunata0118@gmail.com>
2023-06-26 19:33:54 +0700

    eps 22 - selesai

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -8,7 +8,7 @@
   </div>
 
   <div class="col-lg-8">
-    <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="mb-5">
+    <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="mb-5" enctype="multipart/form-data">
 
       @method('put')
       @csrf
@@ -44,6 +44,21 @@
         </select>
       </div>
       <div class="mb-3">
+        <label for="image" class="form-label @error('image') is-invalid @enderror">Post Image</label>
+        <input type="hidden" name="oldImage" value="{{ $post->image }}">
+        @if ($post->image)
+          <img src="{{ asset('storage/' . $post->image) }}" alt="" class="img-preview img-fluid mb-3 col-sm-5 d-block">                        
+        @else
+          <img src="" alt="" class="img-preview img-fluid mb-3 col-sm-5">            
+        @endif
+        <input class="form-control" type="file" id="image" name="image" onchange="previewImage()">
+        @error('image')
+          <div class="invalid-feedback">
+            {{ $message }}
+          </div>
+        @enderror
+      </div>
+      <div class="mb-3">
         {{-- Insert Trix-Editor --}}
         <label for="body" class="form-label">Body</label>        
         @error('body')
@@ -72,6 +87,20 @@
   document.addEventListener('trix-file-accept', function(e){
     e.preventDefault();
   })
+
+  function previewImage() {
+    const image = document.querySelector('#image');
+    const imgPreview = document.querySelector('.img-preview');
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+    oFReader.onload = function(oFREvent) {
+      imgPreview.src = oFREvent.target.result;
+    }
+  }
 
 </script>
 
